3b3f1861ae6ff23e5450d2b08cdaabba5bae22b2
mickey_lifestyle <mike2serg@gmail.com>
2018-01-25 11:22:51 +0100

    Input validation for adding country

--- pages/regions/addcountry.js
+++ pages/regions/addcountry.js
@@ -11,9 +11,11 @@ var country = {
 
     createCountry: function () {
         $("#preloaderNav").show();
-
+// country.inputAlphabet();
 
         var countryName = $("#regionCountry").val();
+        // country.inputAlphabet("123456");
+
         var countryData = {country: countryName};
         console.log(countryName);
 
@@ -27,29 +29,41 @@ var country = {
             console.log(country);
             $("#preloaderNav").hide();
 
-            if(country.error_code === 1){
+            if (country.error_code === 1) {
                 toastr.error(country.message);
-            }else{
+            } else {
                 toastr.success(country.message);
             }
 
         })
-    }
-    // showNotificationSuccess: function () {
-    //     toastr.options = {"positionClass": "toast-bottom-right", "timeOut": "50000000"}
-    //     toastr.success("message");
-    // },
-    //
-    // showNotificationFailed: function () {
-    //     toastr.options = {"positionClass": "toast-bottom-right", "timeOut": "50000000"}
-    //     toastr.error("helloe");
-    // },
-    //
-    // showNotificationInfo: function (message) {
-    //     toastr.options = {"positionClass": "toast-bottom-right", "timeOut": "50000000"}
-    //     toastr.info(message);
-    // }
+    },
 
+    validateAlphabet: function (inputtext) {
+        var alphaExp = /^[a-zA-Z]+$/;
+        if (inputtext.match(alphaExp)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    validateInput: function () {
+        var countryName = $("#regionCountry").val();
+        //
+        // console.log("Validate Input Value");
+        // console.log(country.validateAlphabet(countryName));
+
+        var countryisValid = country.validateAlphabet(countryName);
+        // console.log("Validating Country Value");
+        // console.log(countryisValid);
+
+        if (countryisValid) {
+           // console.log("country is valid");
+            country.createCountry();
+        } else {
+            toastr.warning("Invalid Country Name");
+        }
+    }
 
 }
 
